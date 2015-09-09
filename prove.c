ddb524858be7ca31785a967f18bba6686b182744
user <zppratt@gmail.com>
2015-09-09 13:44:33 -0400

    successful result

--- prove.c
+++ prove.c
@@ -11,7 +11,7 @@ float get_using_rightmost(float * p) {
 
 int main() {
 	float p = pow(10, -7);
-	printf("We are currently adding to one the number: %f\n", p);
-	printf("The first number came to: %f\n", get_using_leftmost(&p));
-	printf("The second number came to: %f\n", get_using_rightmost(&p));
+	printf("We are currently adding to one the number: %.20f\n", p);
+	printf("The first number came to: %.20f\n", get_using_leftmost(&p));
+	printf("The second number came to: %.20f\n", get_using_rightmost(&p));
 }
