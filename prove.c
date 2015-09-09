913b07bf99951734d40645a17ba5ed0dd8b2554f
user <zppratt@gmail.com>
2015-09-09 14:05:47 -0400

    fixed right/left and added pizzaz

--- prove.c
+++ prove.c
@@ -1,17 +1,24 @@
 #include <stdio.h> 
 #include <math.h>
 
-float get_using_leftmost(float * p) {
+float get_using_rightmost(float * p) {
 	return 1+*p+*p+*p+*p+*p+*p+*p+*p+*p+*p+*p;
 }
 
-float get_using_rightmost(float * p) {
+float get_using_leftmost(float * p) {
 	return ((((((((((1+*p)+*p)+*p)+*p)+*p)+*p)+*p)+*p)+*p)+*p);
 }
 
 int main() {
 	float p = pow(10, -7);
+	float rightmost = get_using_rightmost(&p);
+	float leftmost = get_using_leftmost(&p);
 	printf("We are currently adding to one the number: %.20f\n", p);
-	printf("The first number came to: %.20f\n", get_using_leftmost(&p));
-	printf("The second number came to: %.20f\n", get_using_rightmost(&p));
+	printf("Addition using rightmost associativity came to: %.20f\n", rightmost);
+	printf("Addition using leftmost associativity came to: %.20f\n", leftmost);
+	if (leftmost != rightmost) {
+		printf("\nCongratulations! The results are not the same!\n\n");
+	} else {
+		printf("The results are the same.\n");
+	}
 }
